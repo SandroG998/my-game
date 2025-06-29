8bb272b7f7195a70545a1e9606a2077c508c59a8
SandroG998 <sandrocuckoo@gmail.com>
2025-06-29 14:48:40 +0400

    Update main.py

--- The_game/main.py
+++ The_game/main.py
@@ -1,18 +1,18 @@
 import pygame
 import random
-from time import sleep
 
 pygame.init()
 pygame.mixer.init()
 
-background_music = pygame.mixer.music.load("background_sound.mp3")
-background_music = pygame.mixer.music.play(-1)
+# ხმები
+pygame.mixer.music.load("background_sound.mp3")
+pygame.mixer.music.play(-1)
 
 coin_sound = pygame.mixer.Sound("coin_sound.mp3")
 coin_sound.set_volume(0.5)
-
 win_sound = pygame.mixer.Sound("win_sound.mp3")
 
+# ეკრანი
 WIDTH = 800
 HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -20,8 +20,8 @@ pygame.display.set_caption("Test Pygame")
 
 clock = pygame.time.Clock()
 
+# ფერები და ფონები
 BACKGROUND_COLOR = (255, 255, 255)
-
 background_img = pygame.image.load("Background.png")
 background_img = pygame.transform.scale(background_img, (800, 600))
 
@@ -34,28 +34,30 @@ win_scenerio = pygame.transform.scale(win_scenerio, (800, 600))
 level_2_scenerio = pygame.image.load("level2.png")
 level_2_scenerio = pygame.transform.scale(level_2_scenerio, (800, 600))
 
+# აქ შენი ფოტოთი ჩაანაცვლე
+level_3_scenerio = pygame.image.load("level3.png")
+level_3_scenerio = pygame.transform.scale(level_3_scenerio, (800, 600))
+
 coin_png = pygame.image.load("coin.png")
-coin = pygame.Rect(450, 400, 80, 80)
 coin_png = pygame.transform.scale(coin_png, (100, 100))
 
-score = 0
-font = pygame.font.SysFont(None, 30)
-
-coin_visible = True
-
+# მოთამაშე და მონეტა
 player = pygame.Rect(50, 50, 40, 40)
 player_speed = 5
+coin = pygame.Rect(450, 400, 80, 80)
 
-win_platform = pygame.Rect(500000, 500000, 50, 50)
-
+# კედლები
 walls = [
     pygame.Rect(200, 200, 20, 200),
     pygame.Rect(400, 200, 150, 20),
     pygame.Rect(200, 400, 200, 20)
 ]
 
-old_x = player.x
-old_y = player.y
+# სხვა ცვლადები
+score = 0
+font = pygame.font.SysFont(None, 30)
+coin_visible = True
+win_platform = pygame.Rect(50000, 50000, 50, 50)
 
 red = random.randint(0, 255)
 green = random.randint(0, 255)
@@ -68,13 +70,25 @@ show_level2_screen = False
 level2_screen_shown = False
 level2_start_time = None
 
+# მესამე ლეველის ცვლადები
+level_3 = False
+show_level3_screen = False
+level3_screen_shown = False
+level3_start_time = None
+
 while running:
     clock.tick(60)
+
+    old_x = 50
+    old_y = 50
+
     score_text = font.render(f"Score: {score}", True, (0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # მოძრაობა
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
         player.x -= player_speed
@@ -109,30 +123,42 @@ while running:
         coin.y = random.randint(50, 500)
         coin_sound.play()
 
+    # მონეტა კედელს არ უნდა შეეხოს
     for wall in walls:
         if coin.colliderect(wall):
             coin.x = random.randint(50, 700)
             coin.y = random.randint(50, 500)
 
-    if score >= 3:
-        win_platform = pygame.Rect(700, 200, 70, 70)
-    else:
-        win_platform = pygame.Rect(50000, 400, 80, 80)
+    # მოსაგები პლატფორმის ჩვენება ქულების მიხედვით(ყველა ლეველზე რომ გამოჩნდეს და არ იჩხუბონ თითო ლეველზე)
+    if not level_2 and not level_3:
+        if score >= 3:
+            win_platform = pygame.Rect(700, 200, 70, 70)
+        else:
+            win_platform = pygame.Rect(50000, 400, 80, 80)
+    elif level_2 and not level_3:
+        if score >= 3:
+            win_platform = pygame.Rect(700, 200, 70, 70)
+        else:
+            win_platform = pygame.Rect(50000, 400, 80, 80)
+    elif level_3:
+        if score >= 3:
+            win_platform = pygame.Rect(700, 200, 70, 70)
+        else:
+            win_platform = pygame.Rect(50000, 400, 80, 80)
 
-    if player.colliderect(win_platform) and not win1:
+    # ლეველი 1 -> ლეველი 2
+    if player.colliderect(win_platform) and not level_2 and not level_3:
         win1 = True
         win_sound.play()
         show_level2_screen = True
-        level2_start_time = pygame.time.get_ticks()  # დროის დამახსოვრება
+        level2_start_time = pygame.time.get_ticks()
 
-    # მეორე ლეველის ფოტოს ჩვენება
     if show_level2_screen and not level2_screen_shown:
         screen.blit(level_2_scenerio, (0, 0))
         pygame.display.flip()
-        pygame.time.delay(3000)  # აჩვენებს 3 წამით
+        pygame.time.delay(3000)
         level2_screen_shown = True
         level_2 = True
-        # მეორე დონისთვის მოთამაშის პოზიცია, რომ შეიცვალოს
         player.x = 50
         player.y = 50
         score = 0
@@ -143,11 +169,42 @@ while running:
             pygame.Rect(300, 200, 20, 800),
             pygame.Rect(450, 0, 20, 500),
             pygame.Rect(600, 200, 20, 800),
-            #pygame.Rect(400, 400, 200, 20),
-            #pygame.Rect(400, 400, 200, 20),
         ]
 
+    # ლეველი 2 -> ლეველი 3
+    if player.colliderect(win_platform) and level_2 and not level_3:
+        win_sound.play()
+        show_level3_screen = True
+        level3_start_time = pygame.time.get_ticks()
 
+    if show_level3_screen and not level3_screen_shown:
+        screen.blit(level_3_scenerio, (0, 0))
+        pygame.display.flip()
+        pygame.time.delay(3000)
+        level3_screen_shown = True
+        level_3 = True
+        level_2 = False
+        player.x = 50
+        player.y = 50
+        score = 0
+        coin.x = random.randint(50, 700)
+        coin.y = random.randint(50, 500)
+        walls = [
+            # ესენიც კარგად დაალაგე რომ თამაში კარგად ითამაშებოდეს
+            pygame.Rect(0, 100, 750, 20),
+            pygame.Rect(50, 200, 750, 20),
+            pygame.Rect(0, 300, 750, 20),
+            pygame.Rect(50, 400, 750, 20),
+        ]
+
+    # ლეველი 3 -> მოგება
+    if player.colliderect(win_platform) and level_3:
+        screen.blit(win_scenerio, (0, 0))
+        pygame.display.flip()
+        pygame.time.delay(4000)
+        running = False
+
+    # ეკრანის განახლება
     screen.fill(BACKGROUND_COLOR)
     screen.blit(background_img, (0, 0))
     screen.blit(player_img, (player.x, player.y))
@@ -156,14 +213,11 @@ while running:
     if coin_visible:
         screen.blit(coin_png, (coin.x, coin.y))
 
-        for wall in walls:
-            pygame.draw.rect(screen,(red, green, blue), wall)
+    for wall in walls:
+        pygame.draw.rect(screen, (red, green, blue), wall)
 
-        pygame.draw.rect(screen, (90, 87, 35), win_platform)
+    pygame.draw.rect(screen, (90, 87, 35), win_platform)
 
     pygame.display.flip()
-
-old_x = player.x
-old_y = player.y
 
 pygame.quit()
